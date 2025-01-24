6137e337d7e9794bbeadd2a3e7fb86718a478f0b
Matheus Souza <matheusslg1@gmail.com>
2025-01-24 03:16:36 -0300

    feat: add hyperlink documentation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,14 @@ function Header() {
         <div className={styles.logo}>
           <img src={logo} alt="Logo" />
         </div>
-        <span>Documentação</span>
+        <a 
+          href="https://docs.pokemontcg.io/" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className={styles.link}
+        >
+          Documentação
+        </a>
       </div>
     </div>
   );
